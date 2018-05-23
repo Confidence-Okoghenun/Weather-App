66725cce192684deaa1e6e82311ed0198a5f6916
Confidence Okoghenun <confidenceboi@gmail.com>
2018-05-23 20:18:45 +0100

    Get And Display Weather
    The app uses the FCC weather API to
    get and then display the weather.
    
    *XMLHttpRequest gets the JSON and then
     the data is displayed on the page.
    *This change has made the app more usable.
    *Styling and bug fixes are up next.
    
    [Closes-Bug: #003]

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function geoFindMe() {
   let output = document.getElementById("out");
-
+  let weatherDiv = document.querySelector("#weather");
+  
   if (!navigator.geolocation){
     output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
     return;
@@ -11,6 +12,22 @@ function geoFindMe() {
     let longitude = position.coords.longitude;
 
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`, true);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            if(xmlhttp.status == 200) {
+                let obj = JSON.parse(xmlhttp.responseText);
+                // console.table(obj);
+                if(obj.name === "Shuzenji") success(position);
+                else {
+                  weatherDiv.innerHTML = `<p> ${obj.name}, ${obj.sys.country} <br> ${obj.main.temp}&deg;C <br> ${obj.weather[0].main}`;
+                }
+             }
+        }
+    };
+    xmlhttp.send(null);
     
   }
 
